5613361591dc75d0ab196291d14b3b1f394d997f
mogendi <mogendi.mongare@gmail.com>
2021-10-27 17:31:43 +0300

    heroku retrofit

--- urlshortener/wsgi.py
+++ urlshortener/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for urlshortener project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling  # noqa
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urlshortener.settings')
 
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muze.settings')
+
+application = Cling(get_wsgi_application())
