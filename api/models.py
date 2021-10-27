e8f8da383b4a4248ab4ca2274f721ffa27d39b59
mogendi <mogendi.mongare@gmail.com>
2021-10-27 20:05:10 +0300

    heroku retrofit

--- api/models.py
+++ api/models.py
@@ -1,10 +1,11 @@
-from django.db import models
+from django.db import models, transaction
 import hashlib
 
 # Create your models here.
 class Url(models.Model):
     url_id     = models.CharField(max_length=255, blank=True, null=False, unique=True)
     url_actual = models.CharField(max_length=4096, blank=False, null=False)
+    url_clicks = models.IntegerField(null=False, blank=True, default=1)
 
     def save(self, *args, **kwargs):
         if not self.pk and self.url_id == "" or len(self.url_id) < 1:
@@ -12,4 +13,9 @@ class Url(models.Model):
         super(Url, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.url_id 
+        return self.url_id
+    
+    # Click incrementer
+    def inc(self):
+        with transaction.atomic():
+            self.url_clicks += 1
