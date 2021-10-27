767b1897ecb790b666bf2e142b5943b38b0f855a
mogendi <mogendi.mongare@gmail.com>
2021-10-27 19:19:14 +0300

    heroku retrofit

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ class Url(models.Model):
     url_actual = models.CharField(max_length=4096, blank=False, null=False)
 
     def save(self, *args, **kwargs):
-        if not self.pk:
+        if not self.pk and self.url_id == "" or len(self.url_id) < 1:
             self.url_id = hashlib.md5(self.url_actual.encode('utf-8')).hexdigest()[:8]
         super(Url, self).save(*args, **kwargs)
 
