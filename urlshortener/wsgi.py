33560dc86013d28d03c9d1240a445b7d5d502600
mogendi <mogendi.mongare@gmail.com>
2021-10-27 17:38:06 +0300

    heroku retrofit

--- urlshortener/wsgi.py
+++ urlshortener/wsgi.py
@@ -4,6 +4,6 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling  # noqa
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muze.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urlshortener.settings')
 
 application = Cling(get_wsgi_application())
