0e6a82b2ae7256cbc26d137fe723183dc9b733cd
mogendi <mogendi.mongare@gmail.com>
2021-10-29 16:59:52 +0300

    celery longpoller setup

--- urlshortener/celery.py
+++ urlshortener/celery.py
@@ -7,7 +7,7 @@ from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urlshortener.settings')
-app = Celery('picha')
+app = Celery('longpoller')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
